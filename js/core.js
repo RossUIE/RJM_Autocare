d67a9103d093736ca1e34efeac2821686ccb1866
Ross <r.mcmurray1@hotmail.co.uk>
2021-03-06 17:52:32 +0000

    Fixing render issue

--- js/core.js
+++ js/core.js
@@ -1,4 +1,9 @@
-$(document).ready(function(){
+
+	let carType = "small";
+	let serviceType = "maintenance";
+
+	let serviceData;
+	
 	const navSlide = () => {
 		const burger = document.querySelector('.burger');
 		const nav = document.querySelector('.c-header_links');
@@ -55,25 +60,177 @@ $(document).ready(function(){
 			item.addEventListener('click', event => {
 				const service = event.target.innerText;
 				if(service === "Maintenance Wash") {
-					$(".c-services-content_info").load('../resources/ServicesContent/maintenance.html');
+					$(".c-list-content").load('../resources/ServicesContent/maintenance.html');
 				} else if(service === "Exterior Valet") {
-					$(".c-services-content_info").load('../resources/ServicesContent/exterior-valeting.html');
+					$(".c-list-content").load('../resources/ServicesContent/exterior-valeting.html');
 				} else if(service === "Interior Valet") {
-					$(".c-services-content_info").load('../resources/ServicesContent/interior-valeting.html');
+					$(".c-list-content").load('../resources/ServicesContent/interior-valeting.html');
 				} else if(service === "One Stage Enhancement") {
-					$(".c-services-content_info").load('../resources/ServicesContent/one-stage.html');
+					$(".c-list-content").load('../resources/ServicesContent/one-stage.html');
 				} else {
-					$(".c-services-content_info").load('../resources/ServicesContent/full-correction.html');
+					$(".c-list-content").load('../resources/ServicesContent/full-correction.html');
 				}
 			});
 		})
 	}
 
+
+
+	const handleCarSelection = () => {
+		var btnContainer = document.getElementById("c-car-selection-content");
+		var btns = btnContainer.getElementsByClassName("c-car-selection-content_item");
+	
+		for (var i = 0; i < btns.length; i++) {
+			btns[i].addEventListener("click", function(event) {
+				handlePriceSelection()
+				var current = document.getElementsByClassName("active");
+				current[0].className = current[0].className.replace(" active", "");
+				this.className += " active";
+				getCarType(this.id);
+				
+			});
+		}
+	}
+
+	getCarType = (car) => {
+		return carType = car;
+	}
+
+	getServiceType = (service) => {
+		return serviceType = service;
+	}
+
+	
+
+
+	handleServiceType = (event) => {
+		var btnContainer = document.getElementById("c-services-content_list");
+		var btns = btnContainer.getElementsByClassName("service-item");
+		
+		for (var i = 0; i < btns.length; i++) {
+			btns[i].addEventListener("click", function(event) {
+			  	getServiceType(this.id);
+				  console.log(serviceType)
+			  	handlePriceSelection(event);
+			});
+		}
+	}
+
+	getJSONData = () => {
+		$.getJSON("../resources/prices.json", function(data){
+			serviceData = data
+        }).fail(function(){
+            console.log("An error has occurred.");
+        });
+	}
+
+	handlePriceSelection = () => {
+		let servicePrice = serviceData.maintenance.small;
+		// maintenance
+		if(carType === 'small' && serviceType === 'maintenance') {
+			servicePrice = serviceData.maintenance.small
+		}
+
+		if(carType === 'medium' && serviceType === 'maintenance') {
+			servicePrice = serviceData.maintenance.medium
+		}
+
+		if(carType === 'large' && serviceType === 'maintenance') {
+			servicePrice = serviceData.maintenance.large
+		}
+
+		if(carType === 'suv/jeep' && serviceType === 'maintenance') {
+			servicePrice = serviceData.maintenance.jeep
+		}
+		// end of maintenance
+
+		// start of exterior
+		if(carType === 'small' && serviceType === 'exterior') {
+			servicePrice = serviceData.exterior.small
+		}
+
+		if(carType === 'medium' && serviceType === 'exterior') {
+			servicePrice = serviceData.exterior.medium
+		}
+
+		if(carType === 'large' && serviceType === 'exterior') {
+			servicePrice = serviceData.exterior.large
+		}
+
+		if(carType === 'suv/jeep' && serviceType === 'exterior') {
+			servicePrice = serviceData.exterior.jeep
+		}
+		//end of exterior
+
+		// start of interior
+		if(carType === 'small' && serviceType === 'interior') {
+			servicePrice = serviceData.interior.small
+		}
+
+		if(carType === 'medium' && serviceType === 'interior') {
+			servicePrice = serviceData.interior.medium
+		}
+
+		if(carType === 'large' && serviceType === 'interior') {
+			servicePrice = serviceData.interior.large
+		}
+
+		if(carType === 'suv/jeep' && serviceType === 'interior') {
+			servicePrice = serviceData.interior.jeep
+		}
+		//end of interior
+
+		// start of one-stage
+		if(carType === 'small' && serviceType === 'one-stage') {
+			servicePrice = serviceData.oneStage.small
+		}
+
+		if(carType === 'medium' && serviceType === 'one-stage') {
+			servicePrice = serviceData.oneStage.medium
+		}
+
+		if(carType === 'large' && serviceType === 'one-stage') {
+			servicePrice = serviceData.oneStage.large
+		}
+
+		if(carType === 'suv/jeep' && serviceType === 'one-stage') {
+			servicePrice = serviceData.oneStage.jeep
+		}
+		//end of start of one-stage
+
+		// start of full-correction
+		if(carType === 'small' && serviceType === 'full-correction') {
+			servicePrice = serviceData.fullCorrection.small
+		}
+
+		if(carType === 'medium' && serviceType === 'full-correction') {
+			servicePrice = serviceData.fullCorrection.medium
+		}
+
+		if(carType === 'large' && serviceType === 'full-correction') {
+			servicePrice = serviceData.fullCorrection.large
+		}
+
+		if(carType === 'suv/jeep' && serviceType === 'full-correction') {
+			servicePrice = serviceData.fullCorrection.jeep
+		}
+		//end of start of one-stage
+
+
+
+		let price = document.getElementById("price").innerHTML = servicePrice;
+	}
+
+	window.onload = function() {
+		getJSONData();
+	}
+
 	const app = () => {
 		navSlide();
+		handleCarSelection();
+		handleServiceType();
 		renderServiceContent();
+		handlePriceSelection();
 	}
 
 	app();
-
-})
